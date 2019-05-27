c679b0861d5e1d457ec763c906f8b6c6308b28ac
Sean Chan <sean.chan@sorealprop.com>
2019-05-27 17:35:48 +0800

    Going thru vue docs

--- index.js
+++ index.js
@@ -4,3 +4,35 @@ var app = new Vue({
     message: 'Hello Vue!'
   }
 })
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+})
+
+app3.seen = false;
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
